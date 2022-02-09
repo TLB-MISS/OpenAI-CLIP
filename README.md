66fc4c6e376751eb0ec09a2a7e63bbc49c998017
TLB-MISS <bsw1907@naver.com>
2022-02-09 14:22:01 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# INTRODUCTION
+--------------------------------------------------------------------------
+This is a modification of the OpenAI-CLIP repo of moein-shariatnia.
+The current training dataset supports flicker-8k or flicker-30k, and the image encoder supports Resnet50 or ViT(*vit_base_patch16_384*).
+Text encoder supports only DistilBert like moein-shariatnia.
+
+# ENVIRONTMENT SETTING
+--------------------------------------------------------------------------
+```
+$ virtualenv .venv --python=python3.6
+$ source .venv/bin/activate
+$ pip install -r requirements.txt
+```
+
+# EXECUTTION
+--------------------------------------------------------------------------
++ Pretrain
+	$ python3 pretrain.py
+
++ Inference
+	$ python3 inference.py --qeury={YOUR QUERY}
+
+
+# CAUTION
+--------------------------------------------------------------------------
+You must set(or check) some options in config.py before pretrain & inference
+> ex1) *dataset*(*"8k"* or *"30k"*): Train dataset(flicker-8k or flicker-30k)
+> ex2) *model_name*(*"resnet50"* or *"vit_base_patch16_384"*): Type of image encoder
+> ex3) *pretrain*(*True* or *False*): Decide whether to learn by loading pretrain versions of text encoder(DistilBert) and image encoder(resnet50 or ViT)
+> ex4) *batch_size*: Set according to the capacity of the machine
---
+++ config.py
@@ -0,0 +1,56 @@
+import torch
+
+debug = False
+num_workers = 4
+head_lr = 1e-3
+image_encoder_lr = 1e-4
+text_encoder_lr = 1e-5
+weight_decay = 1e-3
+patience = 1
+factor = 0.8
+epochs = 10
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+text_encoder_model = "distilbert-base-uncased"
+text_embedding = 768
+text_tokenizer = "distilbert-base-uncased"
+max_length = 200
+
+# related with image encoder
+# image size & batch size & image_embedding
+model_name = "vit_base_patch16_384" # "resnet50" or "vit_base_patch16_384"
+if model_name == "resnet50":
+    size = 224
+    batch_size = 32
+    image_embedding = 2048
+elif model_name == "vit_base_patch16_384":
+    size = 384
+    batch_size = 4
+    image_embedding = 1000
+else:
+    raise Exception("Does not support other than resnet50 or vit_base_patch16_384")
+
+# related with dataset
+# dataset path & model path
+dataset = "8k" # "8k" or "30k"
+if dataset == "8k":
+    image_path = "/cmsdata/ssd0/sangwon/flickr_dataset/flickr8k/Images"
+    captions_path = "/cmsdata/ssd0/sangwon/flickr_dataset/flickr8k/captions.txt"
+    model_path = "./8k_best.pt"
+elif dataset == "30k":
+    image_path = "/cmsdata/ssd0/sangwon/flickr_dataset/flickr30k/flickr30k_images"
+    captions_path = "/cmsdata/ssd0/sangwon/flickr_dataset/flickr30k/results.csv"
+    model_path = "./30k_best.pt"
+else:
+    raise Exception("Does not support other than flickr8k or flickr30k")
+model_path = "./" + model_name + "_" + dataset + "_best.pt"
+
+
+pretrained = True # for both image encoder and text encoder
+trainable = True # for both image encoder and text encoder
+temperature = 1.0
+
+# for projection head; used for both image and text encoders
+num_projection_layers = 1
+projection_dim = 256 
+dropout = 0.1
